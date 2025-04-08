10dc7d11ff51c55db4dd610cd0a4120df22d8bc5
Hoang Long <ngvhoanglong@gmail.com>
2025-04-09 05:36:41 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Navbar } from "./components/Navbar/Navbar";
+import "./layout.scss";
+import { HomePage } from "./routes/HomePage/HomePage";
+function App() {
+  return (
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+import "./homePage.scss";
+
+export const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1>
+            Find Real Estate & Get Your Dream Place Find Real Estate & Get Your
+            Dream Place
+          </h1>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+};
